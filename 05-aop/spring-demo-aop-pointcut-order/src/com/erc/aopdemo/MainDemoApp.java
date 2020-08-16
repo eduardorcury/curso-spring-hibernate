47db23d64318dcff2cec92caa7de60e95db2778e
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-08-16 16:33:19 -0400

    Especificando ordem de execução dos métodos com @Order

---
+++ 05-aop/spring-demo-aop-pointcut-order/src/com/erc/aopdemo/MainDemoApp.java
@@ -0,0 +1,36 @@
+package com.erc.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.erc.aopdemo.dao.AccountDAO;
+import com.erc.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		Account account = new Account();
+		
+		accountDAO.addAccount(account, true);
+		accountDAO.doWork();
+		
+		accountDAO.setName("foo");
+		accountDAO.setServiceCode("gold");
+		String name = accountDAO.getName();
+		String code = accountDAO.getServiceCode();
+		
+		membershipDAO.addAccount();
+		membershipDAO.goToSleep();
+		
+		context.close();
+
+	}
+
+}
---
+++ 05-aop/spring-demo-aop-pointcut-order/src/com/erc/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package com.erc.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@Before("com.erc.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=====> @Before advice");
+	}
+	
+}
