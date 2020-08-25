3cf35a9a79b65218e7a076821b45ae1b162d4340
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-08-24 21:48:22 -0400

    Implementando autenticação in memory

---
+++ 06-spring-security/spring-security-demo-01-base-app/src/main/java/com/erc/springdemo/config/DemoSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.erc.springdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// adicionando usuários para autenticação in memory
+		
+		UserBuilder user = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(user.username("John").password("test123").roles("EMPLOYEE"))
+			.withUser(user.username("Mary").password("test123").roles("MANAGER"))
+			.withUser(user.username("Susan").password("test123").roles("ADMIN"));
+
+		
+	}
+	
+}
