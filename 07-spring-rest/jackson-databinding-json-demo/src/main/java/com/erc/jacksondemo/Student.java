852c2872156d6cbd4e8c5b82e53e28aa414f2f24
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-01 15:56:51 -0400

    Ignorando propriedades JSON

--- 07-spring-rest/jackson-databinding-json-demo/src/main/java/com/erc/jacksondemo/Student.java
+++ 07-spring-rest/jackson-databinding-json-demo/src/main/java/com/erc/jacksondemo/Student.java
@@ -1,5 +1,8 @@
 package com.erc.jacksondemo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 	
 	private int id;
