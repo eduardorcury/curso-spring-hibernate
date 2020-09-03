e5ef42f6765dbb8ee4b0a300bebb15147c8bdb15
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-03 14:32:06 -0400

    Controlador teste

---
+++ 07-spring-rest/spring-rest-demo/src/main/java/com/erc/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.erc.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.erc.springdemo")
+public class DemoAppConfig {
+
+}
