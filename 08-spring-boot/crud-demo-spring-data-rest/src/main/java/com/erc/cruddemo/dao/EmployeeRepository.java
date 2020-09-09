1648dcd310ab7bc36d391375c86a08876c64eb19
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-09 16:12:35 -0400

    Usando Spring Data Rest

---
+++ 08-spring-boot/crud-demo-spring-data-rest/src/main/java/com/erc/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.erc.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.erc.cruddemo.entity.Employee;
+
+// @RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
