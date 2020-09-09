8c16b7b9795cdd2809ea6bf2a5b88309c1bee71a
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-09 19:19:42 -0400

    Conexão com banco de dados

---
+++ 08-spring-boot/springboot-thymeleaf-crud/src/test/java/com/erc/springbootthymeleaf/SpringbootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erc.springbootthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
