612fcd8b1a56e25be0e488498f886746034ca4fb
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-09 20:04:10 -0400

    Operações de update e delete

--- 08-spring-boot/springboot-thymeleaf-crud/src/main/java/com/erc/springbootthymeleaf/controller/EmployeeController.java
+++ 08-spring-boot/springboot-thymeleaf-crud/src/main/java/com/erc/springbootthymeleaf/controller/EmployeeController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.erc.springbootthymeleaf.entity.Employee;
 import com.erc.springbootthymeleaf.service.EmployeeService;
@@ -30,7 +33,45 @@ public class EmployeeController {
 		
 		model.addAttribute("employees", employees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+		
+	}
+	
+	@GetMapping("showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+		
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+		
+		Employee employee = employeeService.findById(id);
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+		
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int id) {
+		
+		employeeService.deleteById(id);
+		
+		return "redirect:/employees/list";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		employeeService.save(employee);
+		
+		return "redirect:/employees/list";
 		
 	}
 
--- 08-spring-boot/springboot-thymeleaf-crud/src/main/java/com/erc/springbootthymeleaf/dao/EmployeeRepository.java
+++ 08-spring-boot/springboot-thymeleaf-crud/src/main/java/com/erc/springbootthymeleaf/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.erc.springbootthymeleaf.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.erc.springbootthymeleaf.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	public List<Employee> findAllByOrderByLastNameAsc();
 
 }
